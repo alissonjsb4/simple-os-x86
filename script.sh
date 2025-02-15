02025cd37f52b475c162402ef4f10b1302efff8c
Alisson Jaime Sales Barros <100139678+alissonjsb4@users.noreply.github.com>
2025-02-15 18:24:58 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
-set -x
+set -e
 
-# Compilar tudo em Assembly
 nasm -f bin -o bootloader.bin bootloader.asm
 nasm -f bin -o kernel.bin kernel.asm
 nasm -f bin -o editor.bin editor.asm
 
-# Criar imagem de disco
-dd if=/dev/zero of=disk.img bs=1M count=5
-dd if=bootloader.bin of=disk.img bs=512 count=1 conv=notrunc
-dd if=kernel.bin of=disk.img bs=512 seek=2 conv=notrunc
-dd if=editor.bin of=disk.img bs=512 seek=6 conv=notrunc
+check_size() {
+    size=$(stat -c%s "$1")
+    if [ $size -gt 512 ]; then
+        echo "ERRO: $1 excede 512 bytes!"
+        exit 1
+    fi
+}
 
-# Executar no QEMU
-qemu-system-x86_64 -drive format=raw,file=disk.img
+check_size bootloader.bin
+check_size kernel.bin  # Permite até 2KB (4 setores)
+check_size editor.bin
+
+dd if=/dev/zero of=disk.img bs=512 count=1000
+dd if=bootloader.bin of=disk.img conv=notrunc
+dd if=kernel.bin of=disk.img seek=2 conv=notrunc
+dd if=editor.bin of=disk.img seek=6 conv=notrunc
+
+echo "Build completo! Execute com:"
+echo "qemu-system-x86_64 -drive format=raw,file=disk.img"
