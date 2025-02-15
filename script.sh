20061b036ed4664d65c86dd9a3552c83c31d043c
Alisson Jaime Sales Barros <100139678+alissonjsb4@users.noreply.github.com>
2025-02-15 17:58:17 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
-set -x  # Habilitar modo de debug
+set -x
 
-# Compilar o bootloader
+# Compilar tudo em Assembly
 nasm -f bin -o bootloader.bin bootloader.asm
-
-# Compilar o kernel
 nasm -f bin -o kernel.bin kernel.asm
+nasm -f bin -o editor.bin editor.asm
 
-# Compilar o editor (C)
-i686-elf-gcc -ffreestanding -c editor.c -o editor.o
-ld -Ttext 0x2000 --oformat binary editor.o -o editor.bin
-
-# Criar a imagem de disco
+# Criar imagem de disco
 dd if=/dev/zero of=disk.img bs=1M count=5
-
-# Copiar o bootloader para o setor de inicialização
 dd if=bootloader.bin of=disk.img bs=512 count=1 conv=notrunc
-
-# Copiar o kernel para o setor 2
 dd if=kernel.bin of=disk.img bs=512 seek=2 conv=notrunc
-
-# Copiar o editor para o setor 6
 dd if=editor.bin of=disk.img bs=512 seek=6 conv=notrunc
 
 # Executar no QEMU
