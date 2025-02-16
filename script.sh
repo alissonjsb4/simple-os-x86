e813a550bb286ffb130110a375fca03435f6b12d
Josefflessa <148976170+Josefflessa@users.noreply.github.com>
2025-02-16 19:13:36 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,24 +5,10 @@ nasm -f bin -o bootloader.bin bootloader.asm
 nasm -f bin -o kernel.bin kernel.asm
 nasm -f bin -o editor.bin editor.asm
 
-check_size() {
-    file=$1
-    max=$2
-    size=$(stat -c%s "$file")
-    if [ $size -gt $max ]; then
-        echo "ERRO: $file excede $max bytes!"
-        exit 1
-    fi
-}
-
-check_size bootloader.bin 512
-check_size kernel.bin 2048
-check_size editor.bin 1024
-
-dd if=/dev/zero of=disk.img bs=512 count=1000
+dd if=/dev/zero of=disk.img bs=512 count=1440
 dd if=bootloader.bin of=disk.img conv=notrunc
-dd if=kernel.bin of=disk.img seek=2 conv=notrunc
-dd if=editor.bin of=disk.img seek=6 conv=notrunc
+dd if=kernel.bin of=disk.img bs=512 seek=2 conv=notrunc
+dd if=editor.bin of=disk.img bs=512 seek=6 conv=notrunc
 
 echo "✅ Build completo! Execute com:"
 echo "qemu-system-x86_64 -drive format=raw,file=disk.img"
