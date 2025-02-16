4e1fc449421485fce3b7043633667d481421baa5
Josefflessa <148976170+Josefflessa@users.noreply.github.com>
2025-02-16 19:12:37 -0300

    Update editor.asm

--- editor.asm
+++ editor.asm
@@ -1,170 +1,81 @@
-; editor.asm
 [BITS 16]
 [ORG 0x2000]
 
 start:
-    cli
-    cld
-    mov ax, cs
+    xor ax, ax
     mov ds, ax
     mov es, ax
 
-    ; Inicializa o ponteiro do buffer
-    lea di, [buffer]
-
-    ; Limpa a tela e exibe o cabeçalho
     call clear_screen
-    call show_header
+    mov si, header
+    call print_string
 
-    ; Carrega o arquivo salvo (se existir)
-    call load_file
+edit_loop:
+    call wait_key
 
-editor_loop:
-    call get_char
-
-    cmp al, 0x13
-    je do_save
     cmp al, 0x08
-    je do_backspace
+    je .backspace
     cmp al, 0x0D
-    je do_newline
+    je .newline
+    cmp al, 0x13
+    je .save
 
-    cmp al, 0x20
-    jb editor_loop
+    mov ah, 0x0E
+    int 0x10
+    jmp edit_loop
 
-    stosb
-    call print_char
-    jmp editor_loop
+.backspace:
+    call backspace
+    jmp edit_loop
 
-do_backspace:
-    lea si, [buffer]
-    cmp di, si
-    je editor_loop
-    dec di
-    call backspace_cursor
-    jmp editor_loop
-
-do_newline:
+.newline:
     mov al, 0x0D
-    call print_char
+    int 0x10
     mov al, 0x0A
-    call print_char
-    jmp editor_loop
+    int 0x10
+    jmp edit_loop
 
-do_save:
-    call save_file
-    jmp editor_loop
+.save:
+    mov si, saved_msg
+    call print_string
+    jmp edit_loop
 
 clear_screen:
-    mov ah, 0x06
-    xor al, al
+    mov ax, 0x0600
     mov bh, 0x07
     mov cx, 0x0000
     mov dx, 0x184F
     int 0x10
-
-    mov ah, 0x02
-    xor bh, bh
-    xor dx, dx
-    int 0x10
     ret
 
-show_header:
-    mov si, header
-.header_loop:
-    lodsb
-    test al, al
-    jz .done_header
-    call print_char
-    jmp .header_loop
-.done_header:
-    ret
-
-print_char:
-    mov ah, 0x0E
-    int 0x10
-    ret
-
-get_char:
-    mov ah, 0x00
-    int 0x16
-    ret
-
-backspace_cursor:
-    mov al, 0x08
-    call print_char
-    mov al, ' '
-    call print_char
-    mov al, 0x08
-    call print_char
-    ret
-
-save_file:
+backspace:
     mov ah, 0x03
-    mov al, 1
-    mov ch, 0
-    mov cl, 8
-    mov dh, 0
-    mov dl, 0x80
-    mov bx, buffer
-    push es
-    mov ax, 0x0000
-    mov es, ax
-    int 0x13
-    pop es
-    jc .error
-
-    mov si, saved_msg
-    call print_string
-    ret
-.error:
-    mov si, save_error_msg
-    call print_string
-    ret
-
-load_file:
+    int 0x10
+    dec dl
     mov ah, 0x02
-    mov al, 1
-    mov ch, 0
-    mov cl, 8
-    mov dh, 0
-    mov dl, 0x80
-    mov bx, buffer
-    push es
-    mov ax, 0x0000
-    mov es, ax
-    int 0x13
-    pop es
-    jc .error
-
-    mov si, buffer
-.load_print:
-    lodsb
-    test al, al
-    jz .done_load
-    call print_char
-    jmp .load_print
-.done_load:
-    mov di, si
-    dec di
-    ret
-.error:
+    int 0x10
+    mov al, ' '
+    mov ah, 0x0A
+    int 0x10
     ret
 
 print_string:
     mov ah, 0x0E
-.str_loop:
+.print_loop:
     lodsb
     test al, al
-    jz .done_str
+    jz .done
     int 0x10
-    jmp .str_loop
-.done_str:
+    jmp .print_loop
+.done:
     ret
 
-header db " NanoEdit 1.0", 0x0D,0x0A, "Ctrl+S: Save | Backspace: Delete | Enter: New Line", 0x0D,0x0A, "--------------------------------------------------", 0x0D,0x0A, 0
-saved_msg db 0x0D,0x0A, "[SAVED] Texto salvo com sucesso!", 0x0D,0x0A, 0
-save_error_msg db 0x0D,0x0A, "[ERRO] Falha ao salvar!", 0x0D,0x0A, 0
-buffer times 512 db 0
+wait_key:
+    mov ah, 0x00
+    int 0x16
+    ret
 
-times 1024 - ($ - $$) db 0
+header db 13,10,"Editor Simples (Ctrl+S: Salvar)",13,10,"------------------------------",13,10,0
+saved_msg db 13,10,"[Salvo!]",13,10,0
+
+times 1024-($-$$) db 0
