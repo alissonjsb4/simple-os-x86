8fe56f0f5c9d7cca8be51aac477ca9708c1d001d
Josefflessa <148976170+Josefflessa@users.noreply.github.com>
2025-02-16 11:34:32 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 set -e
 
-# Compilar cada ASM
 nasm -f bin -o bootloader.bin bootloader.asm
 nasm -f bin -o kernel.bin kernel.asm
 nasm -f bin -o editor.bin editor.asm
 
-# Verificar tamanhos
 check_size() {
     file=$1
     max=$2
@@ -17,20 +15,13 @@ check_size() {
     fi
 }
 
-check_size bootloader.bin 512     # 1 setor
-check_size kernel.bin 2048        # 4 setores
-check_size editor.bin 1024        # 2 setores
+check_size bootloader.bin 512
+check_size kernel.bin 2048
+check_size editor.bin 1024
 
-# Criar disco de 1000 setores (512 bytes cada)
 dd if=/dev/zero of=disk.img bs=512 count=1000
-
-# Gravar o bootloader no setor 0
 dd if=bootloader.bin of=disk.img conv=notrunc
-
-# Gravar o kernel a partir do setor 2 (setores 2,3,4,5)
 dd if=kernel.bin of=disk.img seek=2 conv=notrunc
-
-# Gravar o editor a partir do setor 6 (setores 6,7)
 dd if=editor.bin of=disk.img seek=6 conv=notrunc
 
 echo "✅ Build completo! Execute com:"
